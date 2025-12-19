ebec48925b55217fc48f7ac07e305565ea7bb23c
Matheus M. <dallalokomatheus07@gmail.com>
2025-12-19 20:12:53 +0000

    bispo

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
 
-int torre(int linha, int coluna) { 
-    // move a torre 5 casas para direita
-    for (int i = 0; i < 5; i++)
+void torre(int casas) { 
+    // função recursiva 
+    if (casas > 0)
     {
-        printf("Direita\n");
+        printf("Direita\n");  // movimentação da torre
+        torre(casas - 1); 
     }
     
 }
 
-int bispo(int linha, int coluna) {
+void bispo(int casas) { // função recursiva do bispo 
 // move o bispo 5 casas em diagonal
-    int i = 1;
-    while (i <= 5)
-    {
-        printf ("cima, direita\n");
-        i++;
+    if (casas > 0)
+    { // loop externo 
+     for(int i = 0; i < casas; i++){
+        printf("Cima\n"); // move para cima
+
+        for (int j = 0; j < casas; j++)// loop interno 
+        {
+            printf("Direita\n"); //movimento para o lado 
+        }
+        
+     }
     }
-}
-int rainha(int linha) {
-    // anda 8 casas para a esquerda 
-    int i = 1;
     
-    do
-    {
-        printf("esquerda\n");
-        i++;
-    } while (i <= 8);
+
+
+void rainha(int casas) { // funçao recursiva para movimentar a rainha 
+    // anda 8 casas para Cima
+   if (casas > 0)
+   {
+    printf("Cima\n");
+    torre(casas - 1);
+   }
+   
 }
 
-int cavalo(int linha, int coluna) { // função para movimentar o cavalo
+void cavalo(int linha, int coluna) { // função para movimentar o cavalo
     int movimentocompleto = 1; 
 
     while (movimentocompleto--) // repete o moviemnto para cima
